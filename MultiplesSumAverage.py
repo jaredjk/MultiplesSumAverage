8740d49d205705e492bd130ca989d4bd68e7eff4
Jared <jaredjkim@gmail.com>
2017-11-18 02:03:34 -0800

    first commit

---
+++ MultiplesSumAverage.py
@@ -0,0 +1,13 @@
+for i in range(1, 1000, 2):
+    print (i)
+
+for i in range(5, 1000000, 5):
+    print (i)
+
+a = [1,2,5,10,255,3]
+b = sum(a)
+print (b)
+
+a = [1,2,5,10,255,3]
+b = sum(a)/len(a)
+print (b)
